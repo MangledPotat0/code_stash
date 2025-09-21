a96616e13a2ebbc35abed2de213f17a2941a8dcf
Dawith Lim <lim185@purdue.edu>
2025-09-21 09:49:44 -0400

    Learned how to make a progress bar

---
+++ bash_scripts/example_progressbar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+progress-bar() {
+	local i=$1
+	local len=$2
+	local j
+
+	local bar_limit=60
+	local percent_done=$((i*100/len))
+	local bar_count=$((percent_done*bar_limit/100))
+	local s="["
+	for ((j=0; j < bar_count; j++)); do
+	    s+="|"
+	done
+	for ((j=bar_count; j < bar_limit; j++)); do
+	    s+=" "
+	done
+	s+="]"
+	echo -ne "$s $i/$len ($percent_done%)\r"
+}
+
+process_file() {
+	local file=$1
+	sleep .05
+}
+
+shopt -s globstar nullglob
+
+echo 'find files'
+files=(./**/*.txt)
+total=${#files[@]}
+echo "found $total files"
+
+current_file=0
+for file in "${files[@]}"; do
+    progress-bar "$((current_file+1))" "$total"
+    process_file $file
+    ((current_file++))
+done
+echo
