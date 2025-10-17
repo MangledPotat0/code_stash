e9d78c460d64dd3bfe46b826dd11c13dce245521
CephalopodOverlord <30482739+MangledPotat0@users.noreply.github.com>
2025-10-17 12:40:56 -0400

    Update joins.sql

--- sql/joins.sql
+++ sql/joins.sql
@@ -1,3 +1,11 @@
 -- joining two tables using shared Id column (personId) between two tables (Person and Address)
 
 SELECT firstName, lastName, city FROM Person LEFT JOIN Address USING(personId);
+
+-- self-joining to attach manager salary to each row, so that employee salary and manager salary can be compared
+
+SELECT employee.name AS Employee
+FROM Employee employee
+JOIN Employee manager
+ON employee.managerId = manager.id
+WHERE employee.salary > manager.salary;
